589b8a02b6868a394bdccf02dc1c2afcf7de8bd2
rising-dragon401 <risingdragon401@gmail.com>
2024-08-22 14:48:02 +0000

    change pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "wabot-frontend",
         script: "npm",
         args: "start",
-        cwd: "/home/ubuntu/wabot/whatsapp-bot-frontend",
+        cwd: "/home/ubuntu/wabot/frontend",
         watch: true,
         env: {
             NODE_ENV: "production",
